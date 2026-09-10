b29a147fe053601615e57c3c61b779d1f9d43ccd
rockybuilds.aiweb3 <rockybuilding.aiweb3@gmail.com>
2026-09-10 04:33:50 -0700

    remove obsolete site metadata

--- src/data/site/siteData.ts
+++
@@ -1,9 +0,0 @@
-import { SiteData } from '../../types';
-
-/**
- * Authoritative global site metadata
- * Replaces legacy network fetch for public/data/data.json
- */
-export const CANONICAL_SITE_DATA: SiteData = {
-  availablity_date: 'TBD',
-};
