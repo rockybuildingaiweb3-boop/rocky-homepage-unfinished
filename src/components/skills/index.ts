ef6de82cb55d19f89ab61c7f9e5b3af62e7671df
rockybuilds.aiweb3 <rockybuilding.aiweb3@gmail.com>
2026-09-10 04:40:43 -0700

    restore: restore required skills exports

---
+++ src/components/skills/index.ts
@@ -0,0 +1,6 @@
+export { TechLogo } from './TechLogo';
+export { TechLogoConstellation } from './TechLogoConstellation';
+export { SkillsMarquee } from './SkillsMarquee';
+export { SkillsPlanetBackground } from './SkillsPlanetBackground';
+export { resolveSkillIcon } from './iconResolver';
+export type { ResolvedIcon } from './iconResolver';
