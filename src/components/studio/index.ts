782c6e1283a288d11245ffe59e30db94dd4fe233
rockybuilds.aiweb3 <rockybuilding.aiweb3@gmail.com>
2026-09-10 19:19:01 -0700

    remove legacy independent Studio architecture

--- src/components/studio/index.ts
+++
@@ -1,9 +0,0 @@
-export { StudioContainer } from './StudioContainer';
-export { StudioShell } from './StudioShell';
-export { StudioProjects } from './modules/StudioProjects';
-export { StudioBlog } from './modules/StudioBlog';
-export { StudioCareer } from './modules/StudioCareer';
-export { StudioModulePlaceholder } from './modules/StudioModulePlaceholder';
-export * from './components';
-export * from './hooks';
-export * from './types';
