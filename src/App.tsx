b162d840398a580648a1d17dd396ae8874a7534f
rockybuilds.aiweb3 <rockybuilding.aiweb3@gmail.com>
2026-09-10 19:20:05 -0700

    remove Work and independent Studio routing from app entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,20 @@
 import React from 'react';
 import { GlobalShell } from './components/layout';
 import { HomePage } from './pages/HomePage';
-import { StudioShell } from './components/studio';
-import { RouterProvider, useRouter } from './router/RouterContext';
 import { useIsMobile } from './hooks/useIsMobile';
 import { usePreloadAssets } from './hooks/usePreloadAssets';
 
 function PortfolioApp() {
   const isMobile = useIsMobile();
-  const { isStudio } = useRouter();
   const { loadingDone, progress, siteData, workData } = usePreloadAssets();
 
   return (
     <GlobalShell progress={progress} loadingDone={loadingDone} isMobile={isMobile}>
-      {isStudio ? (
-        <StudioShell workData={workData} siteData={siteData} />
-      ) : (
-        <HomePage workData={workData} siteData={siteData} />
-      )}
+      <HomePage workData={workData} siteData={siteData} />
     </GlobalShell>
   );
 }
 
 export default function App() {
-  return (
-    <RouterProvider>
-      <PortfolioApp />
-    </RouterProvider>
-  );
+  return <PortfolioApp />;
 }
